d677eab4a48066cd018949869c3ea513fa21b643
DreamMaoMao <2523610504@qq.com>
2024-03-02 17:55:58 +0800

    add some base option

--- qxdrag.py
+++ qxdrag.py
@@ -3,11 +3,12 @@
 from PyQt5.QtWidgets import QApplication, QWidget, QListWidget,QVBoxLayout
 from PyQt5.QtCore import Qt, QMimeData, QUrl
 from PyQt5.QtGui import QDrag,QPixmap,QCursor,QPainter
-import sys
-
+import sys,os
+import argparse
 
 
 class MyListWidget(QListWidget):
+    global args
     def __init__(self):
         super().__init__()
         self.setDragEnabled(True) 
@@ -15,10 +16,9 @@ class MyListWidget(QListWidget):
         self.setDropIndicatorShown(True) 
 
     def startDrag(self, e):
-
         item = self.currentItem()
         mime_data = QMimeData()
-        mime_data.setUrls([QUrl.fromLocalFile(item.text())])
+        mime_data.setUrls([QUrl.fromLocalFile(item.data(Qt.UserRole))])
         drag = QDrag(self)
         drag.setMimeData(mime_data)
         pixmap = QPixmap(self.viewport().visibleRegion().boundingRect().size())
@@ -31,14 +31,17 @@ class MyListWidget(QListWidget):
         drag.setPixmap(pixmap)
         drag.setHotSpot(self.viewport().mapFromGlobal(QCursor.pos()))
         # when release button,copy file and exit
+        
         drag.exec_(Qt.CopyAction)
-        sys.exit()
+        if args.and_exit:
+            sys.exit()
 
 
 class Window(QWidget):
+    global args
     def __init__(self):
         super().__init__()
-        self.file_path = sys.argv[1]
+        self.file_path = args.path
         self.initUI()
 
     def initUI(self):
@@ -80,7 +83,16 @@ class Window(QWidget):
         """
         self.setStyleSheet(style)
         self.listWidget = MyListWidget()
-        self.listWidget.addItem(self.file_path)
+        if args.basename:
+            show_path = os.path.basename(self.file_path)
+        else:
+            show_path = self.file_path
+
+        self.listWidget.addItem(show_path)
+
+        item = self.listWidget.item(self.listWidget.count()-1)
+        item.setData(Qt.UserRole,self.file_path)
+
         # 设置可以拖拽
         self.listWidget.setDragEnabled(True)
 
@@ -90,9 +102,18 @@ class Window(QWidget):
         self.listWidget.show()
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='generate drag file gui from cli')
+    parser.add_argument('-x', '--and-exit', action='store_true', help='exit after first successful drag or drop')
+    parser.add_argument('-b', '--basename', action='store_true', help='Always show basename of each file')
+    parser.add_argument('-w', '--width', type=int, help='window width', default=400)
+    parser.add_argument('-e', '--height', type=int, help='window height',default=300)
+    parser.add_argument('-p', '--path', type=str, help='file full path')
+
+    args = parser.parse_args()
+
     app = QApplication(sys.argv)
     window = Window()
     # window.setGeometry(50, 50, 50, 50)
-    window.resize(400,300)
+    window.resize(args.width,args.height)
     window.show()
     sys.exit(app.exec_())
