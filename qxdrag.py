a48298aacccc241384b77f6c5aea148388215d22
DreamMaoMao <2523610504@qq.com>
2024-03-02 16:49:02 +0800

    rm useless code

--- qxdrag.py
+++ qxdrag.py
@@ -1,30 +1,25 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from PyQt5.QtWidgets import QApplication, QWidget, QListWidget,QHBoxLayout, QVBoxLayout
-from PyQt5.QtCore import Qt, QMimeData, QUrl,QPoint
+from PyQt5.QtWidgets import QApplication, QWidget, QListWidget,QVBoxLayout
+from PyQt5.QtCore import Qt, QMimeData, QUrl
 from PyQt5.QtGui import QDrag,QPixmap,QCursor,QPainter
-import sys,os
+import sys
 
 
 
 class MyListWidget(QListWidget):
     def __init__(self):
         super().__init__()
-        self.setDragEnabled(True) # 启用拖动功能
-        self.setAcceptDrops(True) # 接受放置操作
-        self.setDropIndicatorShown(True) # 显示放置指示器
+        self.setDragEnabled(True) 
+        self.setAcceptDrops(True) 
+        self.setDropIndicatorShown(True) 
 
     def startDrag(self, e):
-        # super().startDrag(e)
-        # 获取当前选中的 item
+
         item = self.currentItem()
-        # 创建一个 QMimeData 对象，用于存储拖动的数据
         mime_data = QMimeData()
-        # 设置数据为 item 的文本
         mime_data.setUrls([QUrl.fromLocalFile(item.text())])
-        # 使用 QDrag 类，实现拖拽操作
         drag = QDrag(self)
-        # drag.setPixmap(QPixmap("~/tool/clash.png"))
         drag.setMimeData(mime_data)
         pixmap = QPixmap(self.viewport().visibleRegion().boundingRect().size())
         pixmap.fill(Qt.transparent)
@@ -35,7 +30,7 @@ class MyListWidget(QListWidget):
         painter.end()
         drag.setPixmap(pixmap)
         drag.setHotSpot(self.viewport().mapFromGlobal(QCursor.pos()))
-        # 拖拽到目标应用,进行文件拷贝，释放鼠标并且退出
+        # when release button,copy file and exit
         drag.exec_(Qt.CopyAction)
         sys.exit()
 
@@ -93,22 +88,6 @@ class Window(QWidget):
         vbox.addWidget(self.listWidget)
         self.setLayout(vbox)
         self.listWidget.show()
-
-    # def mousePressEvent(self, e):
-    #     # 如果文件路径存在，创建一个 QMimeData 对象，存储文件路径
-
-    #     if os.path.exists(self.file_path):
-    #         mime_data = QMimeData()
-    #         mime_data.setUrls([QUrl.fromLocalFile(self.file_path)])
-    #         # 使用 QDrag 类，实现拖拽操作
-    #         drag = QDrag(self)
-    #         # drag.setPixmap(QPixmap("~/tool/clash.png"))
-    #         drag.setMimeData(mime_data)
-    #         # drag.setHotSpot(QPoint(int(drag.pixmap().width()/2),
-    #         #                         int(drag.pixmap().height())))
-    #         # 拖拽到目标应用,进行文件拷贝，释放鼠标并且退出
-    #         drag.exec_(Qt.CopyAction)
-    #         sys.exit()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
